5b5cc603eb26e7c02977c1aaf61a2fa29cb7f79a
Meet Vora <voram@student.ethz.ch>
2019-05-15 02:21:41 +0200

    Restructure to avoid repetitive cpu->gpu transfers

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ USE_GPU = True
 NUM_SAMPLES= 312188
 DEMO = True
 NAME = "demo"
-MODEL = demo_model
+MODEL = models.demo_model
 
 # Hyper parameters
 NUM_EPOCHS = 5
-BATCH_SIZE = 1
-WORKERS = 1
+BATCH_SIZE = 32
+WORKERS = 4
 LEARNING_RATE = 0.003
 LOG_ITER_FREQ = 10
 SAVE_ITER_FREQ = 2000
 
 # Path
-LOG_PATH = "log/"
-DATA_PATH = None
+LOG_PATH = "./log/example/"
+DATA_PATH = "/cluster/project/infk/hilliges/lectures/mp19/project2/"
 
 SUBMISSION_FILES = [
     "data.py",
--- data.py
+++ data.py
@@ -10,16 +10,17 @@ from torchvision import transforms
 class DataSet(Dataset):
 	def __init__(self, root_dir, image_transforms=None, mode="train", normalize=True):
 		self.root_dir = root_dir
-		self.mode = mode
+		self.train = mode.lower() == "train"
 		self.image_transforms = image_transforms
 		file_names = open(os.path.join(root_dir,"annot","%s_images.txt"%mode)).readlines()
 		self.file_paths = [os.path.join(root_dir, "images", path[:-1]) for path in file_names]
-		self.target = None
 		self.normalize = normalize
 
-		if self.mode.lower() == "train":
+		if self.train:
 			annotations_path = os.path.join(root_dir,"annot","train.h5")
-			self.target = h5py.File(annotations_path, 'r')
+			target = h5py.File(annotations_path, 'r')
+			self.target3d = torch.from_numpy(target['pose3d'][()].astype(np.float32))
+			self.target2d = torch.from_numpy(target['pose2d'][()].astype(np.float32))
 
 		self.mean = torch.from_numpy(np.loadtxt(os.path.join(root_dir,'annot',"mean.txt")).reshape([1, 17, 3]).astype(np.float32))
 		self.std = torch.from_numpy(np.loadtxt(os.path.join(root_dir,'annot',"std.txt")).reshape([1, 17, 3]).astype(np.float32))
@@ -36,14 +37,14 @@ class DataSet(Dataset):
 		if self.image_transforms:
 			image = self.image_transforms(image)
 		
-		if not self.target:
+		if not self.train:
 			return image
 
-		target_3D = torch.from_numpy(self.target['pose3d'][idx].astype(np.float32))
-		target_2D = torch.from_numpy(self.target['pose2d'][idx].astype(np.float32))
+		target_3D = self.target3d[idx]
+		target_2D = self.target2d[idx].flatten()
 
 		if self.normalize:
 			target_3D = (target_3D - self.mean) / self.std
 
-		sample = {'image': image, 'pose3d': target_3D.flatten(), 'pose2d': target_2D.flatten()}
+		sample = {'image': image, 'pose3d': target_3D.flatten(), 'pose2d': target_2D}
 		return sample
